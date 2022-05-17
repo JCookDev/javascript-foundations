5f45558314207fb27d8c7ad737d3110c795fbb75
Jeffrey Cook <cookturingedu@gmail.com>
2022-05-17 11:51:00 -0400

    Pass dragon-test for dragon implementation function

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+  constructor(name, rider, hungry){
+    this.name = name;
+    this.rider = rider;
+    this.meals = 1;
+    this.hungry = true;
+  }
 
+  greet(message) {
+    message = this.rider;
+    return `Hi, ${message}!`
+  }
+  eat() {
+    if (this.meals < 3 ) {
+      this.hungry = true;
+      this.meals += 1;
+    } else {
+      this.hungry = false;
+    }
+  }
+}
+
+module.exports = Dragon;
