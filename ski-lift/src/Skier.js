b9beb21762dff82fcfdc21df222314585c85c2a2
Jeffrey Cook <cookturingedu@gmail.com>
2022-05-27 10:47:27 -0400

    Pass all tests in Skier.js file

--- ski-lift/src/Skier.js
+++ ski-lift/src/Skier.js
@@ -1,5 +1,21 @@
 class Skier {
+  constructor (name){
+    this.name = name;
+    this.hasLiftTicket = true;
+    this.skillLevel = 1;
+    this.nextSlope = 'green circle';
+  }
+  takeLesson() {
+    this.skillLevel += 1;
+  }
 
+  pickSlope() {
+    if (this.skillLevel >= 2 && this.skillLevel > 4) {
+      this.nextSlope = 'black diamond';
+    } else {
+      this.nextSlope = 'blue square';
+    }
+  }
 }
 
 module.exports = Skier;
